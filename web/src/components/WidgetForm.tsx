8aa2ac1d51f8092c9b18573ff5535899107c85e8
FelipeEnigmaRock <felj.gomes@gmail.com>
2022-05-04 21:57:29 -0300

    começando formulário widget

---
+++ web/src/components/WidgetForm.tsx
@@ -0,0 +1,17 @@
+export function WidgetForm() {
+    return (
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-4rem)] md:w-auto">
+            <header>
+                <span className="text-xl leading-6">Deixe seu feedback</span>
+            </header>
+
+
+            <p>Hello World</p>
+
+
+            <footer className="text-xs">
+                Feito por <a className="underline underline-offset-2" href="https://github.com/FelipeEnigmaRock"> Felipe Gomes </a>
+            </footer>
+        </div>
+    )
+}
