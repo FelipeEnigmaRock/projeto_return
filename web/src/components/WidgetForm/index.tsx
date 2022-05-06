0986300476792c39f7b05b541a33b76dc619d5e2
FelipeEnigmaRock <felj.gomes@gmail.com>
2022-05-06 17:14:54 -0300

    formulário do widget
    Parte visual completa

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,9 +1,10 @@
 import { ClosedButton } from "../CloseButton";
-
 import bugImageUrl from '../../img/bug.svg';
 import ideaImageUrl from '../../img/idea.svg';
 import thoughtImageUrl from '../../img/thought.svg';
 import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
 export const feedbackTypes = {
     BUG: {
@@ -33,18 +34,26 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+
+    function handleRestartFeedback() {
+        setFeedbackType(null);
+    }
+    
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-4rem)] md:w-auto">
-            <header>
-                <span className="text-xl leading-6">Deixe seu feedback</span>
-
-                <ClosedButton />
-            </header>
-
+            
+            {!feedbackType ? (
+                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+            ) : (
+                <FeedbackContentStep
+                 feedbackType={feedbackType}
+                 onFeedbackRestartRequested={handleRestartFeedback}
+                 />
+            )}
             
 
             <footer className="text-xs">
-                Feito por <a className="underline underline-offset-2" href="https://github.com/FelipeEnigmaRock"> Felipe Gomes </a>
+                Feito com suor por <a className="underline underline-offset-2" href="https://github.com/FelipeEnigmaRock"> Felipe Gomes </a>
             </footer>
         </div>
     )
