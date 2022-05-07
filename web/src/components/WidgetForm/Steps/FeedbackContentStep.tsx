432235333e48b7016b5db9ea130ab3847fe8ea41
FelipeEnigmaRock <felj.gomes@gmail.com>
2022-05-06 23:40:51 -0300

    criando função de printscreen

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,8 @@
 import { ArrowLeft, Camera } from "phosphor-react";
+import { useState } from "react";
 import { FeedbackType, feedbackTypes } from ".."
 import { ClosedButton } from "../../CloseButton"
+import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackTypeStepProps {
     feedbackType: FeedbackType;
@@ -11,6 +13,8 @@ export function FeedbackContentStep({
     feedbackType,
     onFeedbackRestartRequested
 }: FeedbackTypeStepProps) {
+    const [screenshot, setScreenshot] = useState<string | null>(null)
+
     const feedbackTypeInfo = feedbackTypes[feedbackType];
 
 
@@ -40,13 +44,9 @@ export function FeedbackContentStep({
                 />
 
                 <footer className="flex gap-2 mt-2">
-                    <button
-                    type="button"
-                    className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors">
-
-                        <Camera className="w-6 h-6" />
-
-                    </button>
+                    <ScreenshotButton 
+                        onScreenshotTook={setScreenshot}
+                    />
 
                     <button
                     type="submit"
