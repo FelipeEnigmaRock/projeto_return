ccadb9c08165de1cf5cce9a3dc4a8644aebafce2
FelipeEnigmaRock <felj.gomes@gmail.com>
2022-05-07 17:42:48 -0300

    Semi-finalizado front do projeto
    Falta somente arrumar um erro que esta dando

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -5,6 +5,7 @@ import thoughtImageUrl from '../../img/thought.svg';
 import { useState } from "react";
 import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
 import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSucessStep } from "./Steps/FeedbackSucessStep";
 
 export const feedbackTypes = {
     BUG: {
@@ -34,23 +35,30 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm() {
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
+    const [feedbackSent, setFeedbackSent] = useState(false);
 
     function handleRestartFeedback() {
+        setFeedbackSent(false);
         setFeedbackType(null);
     }
-    
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-4rem)] md:w-auto">
-            
-            {!feedbackType ? (
-                <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+            {feedbackSent ? (
+                <FeedbackSucessStep onFeedbackRestartRequested={handleRestartFeedback} />
             ) : (
-                <FeedbackContentStep
-                 feedbackType={feedbackType}
-                 onFeedbackRestartRequested={handleRestartFeedback}
-                 />
+                <>
+                    {!feedbackType ? (
+                        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
+                    ) : (
+                        <FeedbackContentStep
+                            feedbackType={feedbackType}
+                            onFeedbackRestartRequested={handleRestartFeedback}
+                            onFeedbackSent={() => setFeedbackSent}
+                        />
+                    )}
+                </>
             )}
-            
 
             <footer className="text-xs">
                 Feito com suor por <a className="underline underline-offset-2" href="https://github.com/FelipeEnigmaRock"> Felipe Gomes </a>
